d75e73a60e592df618e059f15a940538b4afabd8
b_dhoha <bwb-boutera-d@fondespierre.com>
2025-03-09 22:17:27 +0100

    adding code to model controller and router users

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const connectDB = require("./src/config/db");
+const usersRouters = require ("./src/routers/users.routers");
 
 const PORT = process.env.PORT || 3000;
-
 
 const api = express();
 api.use(express.json()); 
 
-connectDB();
+api.use("/users", usersRouters);
 
+connectDB();
 
 api.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,2 +1,42 @@
 const User = require("../models/users.model");
 
+const getAll = async (req, res) => {
+    try {
+        const allUsers = await User.find();
+        res.json(allUsers);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+const createUsers = async (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        age: req.body.age,
+    });
+    try {
+        const newUser = await user.save();
+        res.status(201).json(newUser);  
+    } catch(error) {
+        res.status(400).json ({ message: error.message });
+    }
+};
+
+const getById = async (req, res) => {
+    try {
+        const userById = await User.findById(req.params.id);
+        if (userById == null) {
+            return res.status(404).json({ message: "User not found" });
+        } 
+        res.json(userById);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+module.exports = {
+    getAll,
+    createUsers,
+    getById,
+}
